c092a0108632cb7e2f0163459f97d964f0bc7659
diegoosorio <diegoosorioupc@gmail.com>
2023-06-27 16:58:21 -0500

    creating tests

---
+++ Mecanillama.API/UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mecanillama.API.UnitTests;
+
+[TestClass]
+public class ApiTests
+{
+    [TestMethod]
+    public void GetEndpoint_ReturnsOk()
+    {
+        // Configurar el escenario de prueba
+
+        // Hacer una solicitud al endpoint GET
+
+        // Verificar el código de estado de respuesta esperado
+        //Assert.AreEqual(200, response.StatusCode);
+    }
+}
