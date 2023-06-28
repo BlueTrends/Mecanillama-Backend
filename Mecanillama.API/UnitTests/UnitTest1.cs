8ff1da20c0e8181117c510cb3c1f8628b1be2c14
diegoosorio <diegoosorioupc@gmail.com>
2023-06-28 01:26:02 -0500

    authentication implementation in tests

--- Mecanillama.API/UnitTests/UnitTest1.cs
+++ Mecanillama.API/UnitTests/UnitTest1.cs
@@ -1,7 +1,10 @@
 using System.Net;
 using System.Net.Http.Headers;
+using System.Text;
+using Mecanillama.API.Security.Domain.Services.Communication;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 
 namespace Mecanillama.API.UnitTests;
 
@@ -20,7 +23,7 @@ public class ApiTests
         client = webAppFactory.CreateClient();
         
         // Obtener el token de autorización
-        string accessToken = ObtenerAccessToken();
+        string accessToken = ObtenerAccessToken().GetAwaiter().GetResult();
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
     }
     
@@ -48,14 +51,38 @@ public class ApiTests
         client.Dispose();
     }
     
-    private string ObtenerAccessToken()
+    private async Task<string> ObtenerAccessToken()
     {
-        // Aquí debes implementar la lógica para obtener el token de autorización
-        // Esto puede implicar hacer una solicitud de autenticación a tu API
-        // o interactuar con algún servicio de autenticación (como Identity Server, Azure AD, etc.)
-        // y extraer el token de autorización necesario para las pruebas
+        
 
-        // Ejemplo básico: retornar un token fijo para propósitos de prueba
-        return "tu_token_de_autorizacion";
+        // Crea una instancia de la clase AuthenticateRequest para enviar las credenciales de autenticación
+        var authenticateRequest = new AuthenticateRequest
+        {
+            Email = "lucas@gmail.com",
+            Password = "1234"
+        };
+
+        // Serializa el objeto AuthenticateRequest a formato JSON
+        var jsonRequest = JsonConvert.SerializeObject(authenticateRequest);
+
+        // Crea un objeto HttpContent con el contenido JSON
+        var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+        // Realiza una solicitud HTTP POST al endpoint de autenticación para obtener el token de acceso
+        var response = await client.PostAsync("api/v1/users/sign-in", content);
+
+        // Verificar el código de estado de respuesta esperado
+        response.EnsureSuccessStatusCode();
+
+        // Lee la respuesta JSON del servidor
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        // Deserializa la respuesta JSON en un objeto AuthenticateResponse
+        var authenticateResponse = JsonConvert.DeserializeObject<AuthenticateResponse>(jsonResponse);
+
+        // Retorna el token de acceso
+        return authenticateResponse.Token;
     }
+    
+    
 }
